19e56b181a2737222dda778ff5c59515f92bc4af
Susan Clay <susanclay333@gmail.com>
2026-05-02 00:25:04 -0700

    added logging/bb graph

--- gcsopt/branch_bound.py
+++ gcsopt/branch_bound.py
@@ -3,11 +3,49 @@ import heapq
 import cvxpy as cp
 import copy
 
+import graphviz as gv
+
 from gcsopt import (GraphOfConvexSets, GraphOfConicSets)
 from gcsopt.graph_problems.shortest_path import bb_node_shortest_path
 from gcsopt.graph_problems.utils import (define_variables,
     enforce_edge_programs, set_solution)
 
+'''
+logging:
+node: 
+- value of variables at each node after solving relaxation + fixed variables
+- order of visitation
+- upper bound after node solved
+2. branches: line with (var = val) along the line to new node
+
+'''
+
+DEBUG = 1
+iter = 0
+
+
+def makedot(root):
+   
+    dot = gv.Digraph()
+
+    # traverse graph from root
+    visited = []
+    q = [root]
+
+    # import pdb
+    # pdb.set_trace()
+
+    while len(q) > 0:
+        n = heapq.heappop(q)
+        label = "ORD: " + str(n.order) + "\nUB: " + str(n.upper_bound)[:7] + "\nYV_FIXED: " + str(len(n.yv_fixed)) + "\nYE_FIXED: " +  str(len(n.ye_fixed))
+        dot.node(str(n.order), label=label)
+
+        for c in n.children:
+            if c.order == -1: continue #not visited/pruned
+            dot.edge(str(n.order), str(c.order), c.branch_info)
+            heapq.heappush(q, c)
+
+    return dot
 
 
 class Node:
@@ -19,6 +57,11 @@ class Node:
         self.ye_fixed = {}
 
         self.children = []
+
+        # for logging/debugging
+        self.order = -1
+        self.upper_bound = -1
+        self.branch_info = ""
     
     def __init__(self, val, yv, ye):
         self.yv_fixed = {}
@@ -35,6 +78,8 @@ class Node:
 
 
 def shortest_path_conic(conic_graph, conic_source, conic_target, tol=1e-4):
+    global DEBUG
+    global iter
 
     upper_bound = np.inf 
     lower_bound = -np.inf 
@@ -52,15 +97,20 @@ def shortest_path_conic(conic_graph, conic_source, conic_target, tol=1e-4):
                         [e.binary_variable for e in conic_graph.edges])))
 
     while (True):
+        iter+=1
         print("upper_bound =", upper_bound)
         print("queue length:", len(leaf_nodes))
         #1. Termination check: if leaf_nodes is empty 
         if (len(leaf_nodes) == 0):
+            if DEBUG: makedot(root).render('BB_trace')
             return #solution already set, anything else needs to be done?
 
         #2. Choose next node, pop it from leaf_nodes
         priority, n = heapq.heappop(leaf_nodes)
-        print("priority/value:", priority)
+        if root is None: root = n
+        n.order = iter
+        n.upper_bound = upper_bound
+       # print("priority/value:", priority)
 
         #3. solve the LP at this node, conditioned on fixed variables. 
             # unbounded --> stop, OG is unbounded
@@ -69,6 +119,8 @@ def shortest_path_conic(conic_graph, conic_source, conic_target, tol=1e-4):
             print("Problem is unbounded!")
             return
         
+       # print("num vars:", len(n.yv) + len(n.ye))
+
         n.set_solution(conic_graph.value, # TODO give nodes custom priority
                         [v.binary_variable for v in conic_graph.vertices], 
                         [e.binary_variable for e in conic_graph.edges])
@@ -86,6 +138,7 @@ def shortest_path_conic(conic_graph, conic_source, conic_target, tol=1e-4):
                 and np.allclose(np.array(ye_values), np.rint(np.array(ye_values)), atol=tol)):
             upper_bound = n.value
             leaf_nodes = [leaf for leaf in leaf_nodes if not (leaf[1].value >= upper_bound)]
+            n.upper_bound = upper_bound
             continue
         
 
@@ -106,23 +159,37 @@ def shortest_path_conic(conic_graph, conic_source, conic_target, tol=1e-4):
         #TODO allow variable number of children
         child0 = copy.deepcopy(n)
         child1 = copy.deepcopy(n)
+        child0.order = -1
+        child1.order = -1
 
-        import pdb
-        pdb.set_trace()
+        # import pdb
+        # pdb.set_trace()
 
         if best_v < best_e:
+            child0.branch_info = "yv[" + str(iv) + "]: " + str(child0.yv[iv].value)[:7] + " --> 0"
+            child1.branch_info = "yv[" + str(iv) + "]: " + str(child1.yv[iv].value)[:7] + " --> 1"
+            print("best vertex", iv, "is this close to 0.5:", best_v)
+
             child0.yv_fixed[iv] = 0
             child1.yv_fixed[iv] = 1
-            print("best vertex", iv, "is this close to 0.5:", best_v)
+            
+            
         else:
+            child0.branch_info = "ye[" + str(ie) + "]: " + str(child0.ye[ie].value)[:7] + " --> 0"
+            child1.branch_info = "ye[" + str(ie) + "]: " + str(child1.ye[ie].value)[:7] + " --> 1"
+            print("best edge", ie, "is this close to 0.5:", best_e)
+            
             child0.ye_fixed[ie] = 0
             child1.ye_fixed[ie] = 1
-            print("best edge", ie, "is this close to 0.5:", best_e)
 
         #TODO could do different priority
+        n.children = [child0, child1]
         heapq.heappush(leaf_nodes, (priority, child0))
         heapq.heappush(leaf_nodes, (priority, child1))
- 
+    
+        print()
+    
+
 
 def shortest_path(graph, source, target):
     conic_graph = graph.to_conic()
